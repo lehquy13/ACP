26838e8cc78a08e7817bbcae2b6a9b827a941d3c
phatlam1811 <phatlam1811@gmail.com>
2023-11-29 05:32:35 +0700

    Add IMediator

---
+++ ACP.Mediator.Abstraction/IMediator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACP.Mediator.Abstraction;
+
+public interface IMediator
+{
+    Task SendAsync<T>(T request, CancellationToken cancelToken) where T : class, IRequest;
+
+    Task<TRes> SendAsync<TRes>(IRequest<TRes> request, CancellationToken cancelToken);
+}
---
+++ ACP.Mediator.Abstraction/IRequestHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACP.Mediator.Abstraction;
+
+public interface IRequestHandler<in TReq> where TReq : class, IRequest
+{
+    Task HandleAsync(TReq request, CancellationToken cancelToken = default);
+}
+
+public interface IRequestHandler<in TReq, TRes> where TReq : class, IRequest<TRes>
+{
+    Task<TRes> HandleAsync(TReq request, CancellationToken cancelToken = default);
+}
